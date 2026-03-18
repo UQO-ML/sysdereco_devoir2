395a2c1468fd61313f575cee36e777bd34b01731
Sloan Kodelowski <77404374+Skodelowski@users.noreply.github.com>
2026-03-18 19:19:58 -0400

    14 tâche 0 sous issue 06 construction des représentations d'items (#40)
    * corrected two stupid mistake
    
    * bases for text representation
    
    * Improved text representation scripts
    
    * Fixed some perf issues
    
    * Managed structured attributes
    
    * Added characterization
    
    * Cleaned item representations
    
    ---------
    
    Co-authored-by: kiloutyg <florian@dkhissi.me>

---
+++ scripts/user_profile.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+from ast import Dict
+import gc
+import os
+
+
+
+class user_profile:
+    
+    def
+
+
+
+
+
+
+
+
+
+
+
+def main() -> None:
+    t_start = time.time()
+    result = user_profile(
+        verbose=True,
+    )
+
+    if result:
+        cli_print_results(result, t_start)
+
+
+
+
+if __name__ == "__main__":
+    main()
